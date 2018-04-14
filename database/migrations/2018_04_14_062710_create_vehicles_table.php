d3393fbd42abaf6e42b7d8da535af32204edd4ce
Martha Salamat <salamat.martha@gmail.com>
2018-04-14 15:45:43 +0800

    vehicle migration

--- database/migrations/2018_04_14_062710_create_vehicles_table.php
+++ database/migrations/2018_04_14_062710_create_vehicles_table.php
@@ -15,6 +15,16 @@ class CreateVehiclesTable extends Migration
     {
         Schema::create('vehicles', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('make');
+            $table->string('model');
+            $table->year('year');
+            $table->string('color');
+            $table->integer('seatingCapacity');
+            $table->string('engineNumber');
+            $table->string('chassisNumber');
+            $table->string('plateNumber');
+            $table->double('rentalRate');
+            $table->integer('user_id');
             $table->timestamps();
         });
     }
