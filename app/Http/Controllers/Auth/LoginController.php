bc1e4b9909d6565720f354c6e948fe0c7389e541
Martha Salamat <salamat.martha@gmail.com>
2018-04-18 07:08:17 +0800

    update

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -29,7 +29,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/dashboard';
+    protected $redirectTo = '/';
 
 
     protected $redirectAfterLogout = '/';
